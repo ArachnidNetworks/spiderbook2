7c48bdc8dbcfdac16ee31e8b8334c7d17149af7e
nicey <nicey@protonmail.ch>
2019-09-11 17:06:02 -0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,25 @@
+import hashlib, datetime
+import psycopg2
+
+# Password hasher
+def hashpass(password):
+    return hashlib.sha1(password.encode()).hexdigest()[:24]
+
+conn = psycopg2.connect("dbname=spiderbook user=postgres")
+c = conn.cursor()
+
+# Get posts with an optional category filter
+def get_posts(category=None):
+    if not category:
+        c.execute("SELECT * FROM posts")
+    else:
+        c.execute("SELECT * FROM posts WHERE category = %s", (category,))
+
+    # Make a list of dicts with the keys being the rows and each row from c.fetchall() being
+    # the values
+    els = []
+    rows = c.fetchall()
+    colnames = [desc.name for desc in c.description]
+    for row in rows:
+            els.append(dict(zip(colnames, list(row))))
+    return els
---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect, make_response, render_template, url_for
+import os, time
+import api
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ["SECRET"]
+
+@app.route("/")
+def home():
+    return "<h1>Home</h1>"
+
+@app.route("/posts")
+def posts():
+    posts = api.get_posts()
+    return "<h1>Posts</h1>"
+
+@app.route("/posts/<category>")
+def catpost(category):
+    posts = api.get_posts(category)
+    return f"<h1>'{category}' posts</h1>"
+
+@app.route("/posts/create", methods=['GET', 'POST'])
+def create_post():
+    return "<h1>Create post</h1>"
+
+@app.route("/user/signin")
+def signin():
+    return "<h1>Sign in</h1>"
+
+@app.route("/user/signup")
+def signup():
+    return "<h1>Sign up</h1>"
+
+if __name__ == "__main__":
+    app.run(
+        host="localhost",
+        port=8000,
+        debug=True
+    )
