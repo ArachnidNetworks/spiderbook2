6c6ac49f6914b891a698b2f5023f699b241b0224
nicey <nicey@protonmail.ch>
2019-09-17 15:13:54 -0300

    Added mobile touch effect to buttons

--- static/js/default.js
+++ static/js/default.js
@@ -1,12 +1,23 @@
-function ctrl_aside(state) {
+function ctrl_aside(s) {
     const aside = document.querySelector("#popular-cats-a");
     const open_aside = document.querySelector("#open-aside");
-    if (state) {
+    if (s) {
         aside.classList.add("closed");
         open_aside.classList.add("oa-active");
     }
     else {
         aside.classList.remove("closed");
         open_aside.classList.remove("oa-active");
+    }
+}
+
+function clickpc(cat, s) {
+    if (s) {
+        cat.classList.add("touched");
+    }
+    else {
+        setTimeout(() => {
+            cat.classList.remove("touched");
+        }, 80);
     }
 }
