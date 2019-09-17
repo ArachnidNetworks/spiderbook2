0ee8ceb416cecaa3a9b914028eba74219650f89a
nicey <nicey@protonmail.ch>
2019-09-17 11:17:58 -0300

    Added button to reopen aside

--- static/js/default.js
+++ static/js/default.js
@@ -1,9 +1,12 @@
 function ctrl_aside(state) {
     const aside = document.querySelector("#popular-cats-a");
+    // const open_aside = document.querySelector("#open-aside");
     if (state) {
         aside.classList.add("closed");
+        //open_aside.classList.add("oa_active");
     }
     else {
         aside.classList.remove("closed");
+        //open_aside.classList.remove("oa-active");
     }
 }
