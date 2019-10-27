7f941e1addbddcf2f5d5fe45861d0edca3b0c6d3
nicey <nicey@protonmail.ch>
2019-10-27 11:06:54 -0300

    Added function to connect to database

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from psycopg2 import connect
 
-conn = connect("dbname=spiderbook user=postgres password=postgres")
-c = conn.cursor()
+def dbsetup(db, user, password):
+    conn = connect(f"dbname={db} user={user} password={password}")
+    c = conn.cursor()
+    return conn, c
+
+conn, c = dbsetup(db="spiderbook", user="postgres", password="postgres")
 
 c.close()
 conn.close()
