a343b18c31e0a630f5d27967ae9b44e2cf1211b8
nicey <nicey@protonmail.ch>
2019-10-27 18:40:42 -0300

    Finished the ID functions

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from psycopg2 import connect
+from random import randint
+from hashlib import sha512
 
 def dbsetup(db, user, password):
     conn = connect(f"dbname={db} user={user} password={password}")
@@ -6,11 +8,31 @@ def dbsetup(db, user, password):
     return conn, c
 conn, c = dbsetup(db="spiderbook", user="postgres", password="postgres")
 
-def new_uid():
-    pass
+def get_idns(restriction=False, arguments=()):
+    query = "SELECT idn FROM idnumbers"
+    if restriction:
+        # if a restriction is imposed, such as a WHERE clause, add it to
+        # the query, else execute it
+        query += " " + restriction
+    c.execute(query, arguments)
+    # Return everything, in the form of a list of tuples
+    return c.fetchall()
 
-def get_uid():
-    pass
+def new_idn():
+    # get the latest (biggest) id number
+    last_id = get_idns(restriction="ORDER BY idn DESC")[0][0]
+    # add a random value between 1 and 10 to it
+    new = last_id + randint(1, 10)
+    query = "INSERT INTO idnumbers (idn) VALUES (%s)"
+    arguments = (new,)
+    c.execute(query, arguments)
+    # conn.commit()
+    # return the latest idn
+    return new
+
+def new_uid(chars):
+    idn = new_idn()
+    return sha512(bytes(idn)).hexdigest()[:chars]
 
 c.close()
 conn.close()
