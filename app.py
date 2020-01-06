cc3d2f860b6bee9ad99735aa6065c9a9bfe4b5e5
nicey <nicey@protonmail.ch>
2020-01-06 17:26:31 -0300

    Added one-liner system to manage requests

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-from flask import Flask, request, redirect, make_response, render_template, url_for, jsonify, abort
-from flask import send_from_directory, flash
-import api
 import os
 from traceback import print_exc
+from typing import Callable
+from flask import Flask, request, redirect, make_response, render_template, url_for, jsonify, abort
+from flask import send_from_directory, flash, Response
+from werkzeug.local import LocalProxy
+import api
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ["SECRET"]
@@ -13,55 +15,44 @@ SERVER = 500
 NOT_FOUND = 404
 FORBBIDEN = 403
 
+# Utility functions
+
+def view_template(request: LocalProxy, api_function: Callable[[LocalProxy], bool], success: tuple, failure: tuple) -> Response:
+    try:
+        result = api_function(request)
+    finally:
+        if result:
+            return make_response(success)
+        else:
+            return make_response(failure)
+
+# View functions
+
 @app.route('/')
 def root():
     return "Hello!"
 
 @app.route('/api/post', methods=['POST'])
-def test_post():
-    try:
-        api.add_post(request)
-        return "Success!", 200
-    except:
-        return "aaaa", 500
+def add_post():
+    return view_template(request, api.add_post, ("Success", 200), ("aaaa", 500))
 
 @app.route('/api/reply', methods=['POST'])
-def test_reply():
-    try:
-        api.reply(request)
-        return "Success!", 200
-    except:
-        return "aaaa", 500
+def reply():
+    return view_template(request, api.reply, ("Success", 200), ("aaaa", 500))
 
 @app.route('/api/signup', methods=['POST'])
 def signup():
-    try:
-        api.signup(request)
-        return "Success!", 200
-    except:
-        return "aaaa", 500
+    return view_template(request, api.signup, ("Success", 200), ("aaaa", 500))
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    try:
-        api.login(request)
-        return "Success!", 200
-    except:
-        return "aaaa", 500
+    return view_template(request, api.login, ("Success", 200), ("aaaa", 500))
 
 @app.route('/api/su/removepost', methods=['POST'])
 def remove_post():
-    try:
-        api.remove_post(request)
-        return "Success!", 200
-    except:
-        return "aaaa", 500
+    return view_template(request, api.remove_post, ("Success", 200), ("aaaa", 500))
 
 @app.route('/api/su/ban', methods=['POST'])
 def ban_ip():
-    try:
-        api.ban_ip(request)
-        return "Success!", 200
-    except:
-        return "aaaa", 500
+    return view_template(request, api.ban_ip, ("Success", 200), ("aaaa", 500))
 
