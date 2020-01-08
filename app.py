636184e278af1f4099513d70b2070eb554976fe4
nicey <nicey@protonmail.ch>
2020-01-08 17:46:22 -0300

    Remove 1-liner function
    Login needs extra Flask code and it can't support it :/

--- app.py
+++ app.py
@@ -15,17 +15,6 @@ SERVER = 500
 NOT_FOUND = 404
 FORBBIDEN = 403
 
-# Utility functions
-
-def view_template(request: LocalProxy, api_function: Callable[[LocalProxy], bool], success: tuple, failure: tuple) -> Response:
-    try:
-        result = api_function(request)
-    finally:
-        if result:
-            return make_response(success)
-        else:
-            return make_response(failure)
-
 # View functions
 
 @app.route('/')
@@ -34,25 +23,61 @@ def root():
 
 @app.route('/api/post', methods=['POST'])
 def add_post():
-    return view_template(request, api.add_post, ("Success", 200), ("aaaa", 500))
+    try:
+        result = api.add_post(request)
+    finally:
+        if result:
+            return make_response(("Success", 200))
+        else:
+            return make_response(("aaaa", 500))
 
 @app.route('/api/reply', methods=['POST'])
 def reply():
-    return view_template(request, api.reply, ("Success", 200), ("aaaa", 500))
+    try:
+        result = api.reply(request)
+    finally:
+        if result:
+            return make_response(("Success", 200))
+        else:
+            return make_response(("aaaa", 500))
 
 @app.route('/api/signup', methods=['POST'])
 def signup():
-    return view_template(request, api.signup, ("Success", 200), ("aaaa", 500))
+    try:
+        result = api.signup(request)
+    finally:
+        if result:
+            return make_response(("Success", 200))
+        else:
+            return make_response(("aaaa", 500))
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    return view_template(request, api.login, ("Success", 200), ("aaaa", 500))
+    try:
+        result = api.login(request)
+    finally:
+        if result:
+            return make_response(("Success", 200))
+        else:
+            return make_response(("aaaa", 500))
 
 @app.route('/api/su/removepost', methods=['POST'])
 def remove_post():
-    return view_template(request, api.remove_post, ("Success", 200), ("aaaa", 500))
+    try:
+        result = api.remove_post(request)
+    finally:
+        if result:
+            return make_response(("Success", 200))
+        else:
+            return make_response(("aaaa", 500))
 
 @app.route('/api/su/ban', methods=['POST'])
 def ban_ip():
-    return view_template(request, api.ban_ip, ("Success", 200), ("aaaa", 500))
+    try:
+        result = api.ban_ip(request)
+    finally:
+        if result:
+            return make_response(("Success", 200))
+        else:
+            return make_response(("aaaa", 500))
 
