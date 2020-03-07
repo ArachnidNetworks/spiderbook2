12c7d5befb8c238d62a6b65adfe2e7570c9507ac
nicey <nicey@protonmail.ch>
2020-03-07 19:31:34 -0300

    Moved API files from /api/ to /

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 # from traceback import print_exc
-from flask import Flask, request, redirect, make_response, render_template, \
-                  url_for, jsonify, abort, flash, Response
-from API import api
+from flask import Flask, request
+# redirect, make_response, render_template, url_for, jsonify, abort, flash
+# Response
+from api import APImgr
+from dbint import DBInterface
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ["SECRET"]
@@ -13,7 +15,21 @@ SERVER = 500
 NOT_FOUND = 404
 FORBBIDEN = 403
 
+db = DBInterface("spiderbook", "postgres", "postgres")
+api = APImgr(db)
+
 
 @app.route('/')
 def root():
     return "Hello!"
+
+
+@app.route('/add/post', methods=['POST'])
+def add_post():
+    fd = request.form
+    data = {"title": fd.get("title"), "parent": fd.get("parent"),
+            "body": fd.get("body")}
+    # try:
+    api.add(data)
+    # except:
+    # print_exc()
